25c9b38cb6b330bf3a23ce329c6450dd2c589630
TicoCR16 <mariojsanchez15@gmail.com>
2020-05-27 11:16:42 -0700

    Starting Point

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
@@ -13,26 +14,48 @@ import { CREATE_ACCOUNT, SIGN_IN } from "./utils/constants";
 
 import './App.css';
 
-const d = [ { title : "HEllo", content : "asdfasf", pinned : false } ];
+const d = [ { title : "HEllo", content : "asdfasf", pinned : true }, { title : "HEllo", content : "asdfasf", pinned : false }, { title : "HEllo", content : "asdfasf", pinned : false }, { title : "HEllo", content : "asdfasf", pinned : false }, { title : "HEllo", content : "asdfasf", pinned : false }, { title : "HEllo", content : "asdfasf", pinned : false }, { title : "HEllo", content : "asdfasf", pinned : false }, { title : "HEllo", content : "asdfasf", pinned : false }, { title : "HEllo", content : "asdfasf", pinned : false }, { title : "HEllo", content : "asdfasf", pinned : true }, { title : "HEllo", content : "asdfasf", pinned : true }, { title : "HEllo", content : "asdfasf", pinned : false } ];
 
 const Header = styled.div`
   color : whitesmoke;
   font-size : 120px;
   text-align : center;
-  margin: 100px;
+  margin: 50px 0 100px;
   font-family: 'Pacifico', cursive;
   letter-spacing : 13px;
+`;
+
+
+const FilterContainer = styled.div`
+  text-align : right;
+  position : relative;
+  right : 150px; 
+
+  button
+  {
+    width : 350px;
+    height : 70px;  
+    border-radius : 30px;
+    color : rgb( 190, 190, 190 );
+    font-size : 26px;
+    color : #004BA8;
+    border : 3px solid #004BA8;
+  }
+
+  button:hover
+  {
+    background : #004BA8;
+  }
 `;
 
 function App() 
 {
   const [ createAccountForm, setCreateAccountForm ] = useState( CREATE_ACCOUNT );
-
   const [ signInForm       , setSignIn            ] = useState( SIGN_IN        );
   const [ alreadyUser      , setAlreadyUser       ] = useState( false          );
-
-  const [ listOfArticles   , setListOfArticles    ] = useState( d             ); 
-
+  const [ filter           , setFiler             ] = useState( false          );
+  const [ listOfArticles   , setListOfArticles    ] = useState( d              ); 
+  
   const onChangeCreate = e => 
   {
     setCreateAccountForm( { ...createAccountForm, [ e.target.name ] : e.target.value } );
@@ -43,8 +66,6 @@ function App()
     setSignIn( { ...signInForm, [ e.target.name ] : e.target.value } );
   }
 
- 
-
   useEffect( () => {
 
   }, [ listOfArticles.length ] )
@@ -52,12 +73,18 @@ function App()
   return (
     <BrowserRouter>
 
-
       <Header>PinteReach</Header>
 
       <Switch>
+        
         <Route path = "/articles">
-          <Articles listOfArticles = { listOfArticles } />
+
+          <FilterContainer>
+            <button>My Articles</button>
+          </FilterContainer>
+
+          { filter ? <Articles listOfArticles = { listOfArticles.filter( article => article.pinned ) } /> : <Articles listOfArticles = { listOfArticles } /> }
+          
         </Route>
 
         <Route path = "/" exact>
@@ -68,9 +95,8 @@ function App()
           />
         </Route>
 
-        {/* onSubmit       = { [ onSubmitSignInForm, onSubmitCreateForm ] }  */}
-
       </Switch>
+
     </BrowserRouter>
   );
 }
