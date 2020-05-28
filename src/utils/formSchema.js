3a98211125bd12a5ce2e12345afbda23a6b4e664
TicoCR16 <mariojsanchez15@gmail.com>
2020-05-28 09:42:48 -0700

    Safe Point

---
+++ src/utils/formSchema.js
@@ -0,0 +1,16 @@
+import * as yup from "yup";
+
+const createAccountForm = yup.object().shape(
+{
+  username : yup.string().trim().min( 2, "Username Must Be Atleast 2 Letters" ).required( "Username Must Be Provided" ),
+  email : yup.string().email( "Enter Correct Email Format" ).required( "Email Must Be Provided" ),
+  password : yup.string().password( "Enter Password" ).required( "Password Must Be Provided" )
+} );
+
+const signInForm = yup.object().shape(
+{
+  username : yup.string().trim().min( 2, "Username Must Be Atleast 2 Letters" ).required( "Username Must Be Provided" ),
+  password : yup.string().password( "Enter Password" ).required( "Password Must Be Provided" )
+} );
+
+export { createAccountForm, signInForm };
